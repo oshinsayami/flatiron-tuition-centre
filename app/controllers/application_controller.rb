666547393a83aa2a5c681844e039b9d6bcb614f1
Simran <mdrsimran@gmail.com>
2020-12-20 15:11:59 -0600

    session updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, 'gghhihhiji7hkhjh'
+    set :session_secret, ENV['SESSION_SECRET']
   end
 
   get "/" do
